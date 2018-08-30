fd69aa8947a27795ae70d97720f6f7f6b12ba0a8
bird4125 <bird4125@naver.com>
2018-08-30 19:46:18 +0900

    132

--- 공모전/common/app.js
+++ 공모전/common/app.js
@@ -1,8 +1,71 @@
-/*var num = 0;
-
-
+var num = 1;
 
 $(document)
+.on("click",".nav_box label:nth-child(1) li",function(){
+	num = 1 ;
+	if (num == 1 ) {
+		$(".bg").css("margin-top","0%");
+		alert("go up");
+	}})
+
+.on("click",".nav_box label:nth-child(2) li",function(){
+	num = 2 ;
+	if (num == 2 ) {
+		$(".bg").css("margin-top","-100vh");
+		alert("go down");
+	}
+
+})
+
+$(window)
+.keydown(function(event){
+	if (event.which == 40) {
+		num = num+1;
+		if (num == 2) {
+			$(".bg").css("margin-top","-100vh");
+			$(".body .move1").css({
+				"transform": "translateY(-2000px)",
+				"transition": "1s",
+				"opacity": 0
+			});
+			$(".body .move2").css({
+				"transform": "inherit",
+				"transition": "1s",
+				"opacity": 1
+			})
+			$(".main .nav .nav_box label:nth-child(1) li").css({
+				"color":"#fff",
+				"font-size": "30px",
+				"transition":".5s",
+				"left": "-3px"
+			})
+			$(".main .nav .nav_box label:nth-child(1) li:after").css({
+				"color":"#fff",
+				"font-size": "12px",
+				"transition":".5s",
+				"left": "10px",
+				"bottom": 0
+			})
+			$(".main .nav .nav_box label:nth-child(2) li").css({
+				"color":"LIGHTGREEN",
+				"font-size": "50px",
+				"transition":".5s",
+				"left": "-6px"
+			})
+			alert("go down");
+		}
+	} else if(event.which == 38) {
+		num = num-1;
+		if(num == 1) {
+			$(".bg").css("margin-top","0%");
+			alert("go up")
+		}
+	}
+	console.log(event.which);
+	console.log(num);
+})
+
+/*$(document)
 .keydown(function(event){
 	if (event.which == 40) {
 		num += 1;
